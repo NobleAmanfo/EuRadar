196ea8a7b9abb5a5e5d7e3f7e89aa88d763efebc
NobleAmanfo <67824486+NobleAmanfo@users.noreply.github.com>
2022-07-26 17:04:25 -0400

    update3

--- Components/BottomNavTabs/Clubs.js
+++ Components/BottomNavTabs/Clubs.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default function Clubs() {
     return (
-      <View>
-        <Text> Clubs Tab </Text>
+      <View style={styles.container1}>
+      <View style = {{flexDirection: 'row',  alignItems: 'center',}}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search Club"
+
+          onChangeText={text => setEmail(text)}
+        />
+        <TouchableOpacity onPress={null}>
+                <Text style={{marginLeft:10, color: '#94a274', fontWeight: "bold" }} >Search</Text>
+        </TouchableOpacity>
       </View>
+    </View>
     )
   }
 
+  const styles = StyleSheet.create({
+    container1: {
+      backgroundColor: '#00001c',
+      alignItems: 'center',
+      flex: 1,
+      borderRadius: 20,
+      justifyContent: 'center'
+    },
+    input: {
+      paddingVertical: 12,
+      width: 300,
+      borderWidth: 1,
+      backgroundColor: 'white',
+      padding: 10,
+      borderRadius: 10
+    },
+  
+  })
--- Components/BottomNavTabs/Players.js
+++ Components/BottomNavTabs/Players.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default function Players() {
-    return (
-      <View style = {styles.container1}>
-        <Text>
-            
-        </Text>
+  return (
+    <View style={styles.container1}>
+      <View style = {{flexDirection: 'row',  alignItems: 'center',}}>
         <TextInput
-                style={styles.input}
-                placeholder="Email Address"
-                
-                onChangeText={text => setEmail(text)}
+          style={styles.input}
+          placeholder="Search Player"
 
-            />
+          onChangeText={text => setEmail(text)}
+        />
+        <TouchableOpacity onPress={null}>
+                <Text style={{marginLeft:10, color: '#94a274', fontWeight: "bold" }} >Search</Text>
+        </TouchableOpacity>
       </View>
-    )
-  }
+    </View>
 
-  const styles = StyleSheet.create({
-    container1: {
-        backgroundColor: '#00001c',
-        alignItems: 'center',
-        flex: 1,
-        borderRadius: 20,
-        justifyContent: 'center'
-    },
-    input: {
-        marginTop: 20,
-        paddingVertical: 12,
-        width: 300,
-        borderWidth: 1,
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10
-    },
-   
+  )
+}
+
+const styles = StyleSheet.create({
+  container1: {
+    backgroundColor: '#00001c',
+    alignItems: 'center',
+    flex: 1,
+    borderRadius: 20,
+    justifyContent: 'center'
+  },
+  input: {
+    paddingVertical: 12,
+    width: 300,
+    borderWidth: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10
+  },
+
 })
